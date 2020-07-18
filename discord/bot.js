58bf4094033f53cfe4c484c9414624aa9fddd2e6
coalpha <22552895+coalpha@users.noreply.github.com>
2020-07-18 01:23:33 -0700

    Some sources and discord bot
    That's enough for tonight

---
+++ discord/bot.js
@@ -0,0 +1,32 @@
+const Eris = require("eris");
+
+const config = require("./config")
+
+/** @type {import("eris").Client} */
+const bot = new Eris(config.bot.token);
+
+bot.on("ready", () => {
+   console.log("I'm a speed junkie...");
+   const guilds = bot.guilds.forEach(guild => {
+      console.log(guild.name);
+   });
+});
+
+/**
+ * @param {import("eris").Message} msg
+ * @returns {boolean}
+ */
+function messageIsFromSelf(msg) {
+   return msg.author.id === config.client.id;
+}
+
+bot.on("messageCreate", msg => {
+   if (messageIsFromSelf(msg)) {
+      // ignore
+   } else {
+      console.log(msg);
+      bot.createMessage(msg.channel.id, "Pong!");
+   }
+});
+
+bot.connect();
